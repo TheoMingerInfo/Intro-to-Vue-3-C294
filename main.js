5775ad9d7e7d1fecc4de825271262768523a1595
Théo Minger <theo.minger@gmail.com>
2025-11-04 16:01:03 +0100

    feat(exo 2 ,3 ,4,5)

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
-    data(){ return{
-        product:'Socks',
-        description:'Des chaussettes bleues'
-    }}
-});
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url:'https://www.etml.ch',
+            inventory: 9,
+            onSale: false,
+            variants: [
+                    { id: 2234, color: 'green' },
+                    { id: 2235, color: 'blue' }
+        ],
+        sizes:['XS','S','M','L','XL']
+    }
+    }
+})
 
