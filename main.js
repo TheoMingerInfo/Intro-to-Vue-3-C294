d273d4c3d5bc5c4b3d5a9ca97345c5a2363bad98
Théo Minger <theo.minger@gmail.com>
2025-11-05 08:24:32 +0100

    feat(6) -> exo 6 sur les boutons

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
@@ -11,5 +11,17 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },methods: {
+        addToCart() {
+        this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },removeFromCart() {
+        if(this.cart > 0){
+            this.cart -= 1
+        }
     }
+    }
+
 })
